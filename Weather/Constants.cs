96544fb625d26a4f56fa8cce6efe05c64f6b48cc
Ali Hussein <alixhsen.2003@gmail.com>
2024-09-18 12:30:56 +0300

    first commit

---
+++ Weather/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+
+        public static string OpenWeatherMapAPIKey = "f030653115e8f9eee1e580d28c0e251b";
+    }
+}
---
+++ Weather/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace Weather
+{
+    public partial class MainPage : ContentPage
+    {
+        RestService _restService;
+        DateTime currentDateTime = DateTime.Now;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _restService = new RestService();
+            _cityEntry.Text = "Beirut";
+             currentDate.Text = currentDateTime.ToString("MMMM dd , hh:mmtt");
+
+        }
+
+        async void OnGetWeatherButtonClicked(object sender, EventArgs e)
+        {
+            string city = string.IsNullOrWhiteSpace(_cityEntry.Text) ? "Beirut" : _cityEntry.Text;
+            WeatherData weatherData = await _restService.GetWeatherData(GenerateRequestURL(Constants.OpenWeatherMapEndpoint, city));
+            BindingContext = weatherData;
+        }
+        string GenerateRequestURL(string endPoint, string city)
+        {
+            string requestUri = endPoint;
+            requestUri += $"?q={city}";
+            requestUri += "&units=imperial";
+            requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+            return requestUri;
+        }
+    }
+}
